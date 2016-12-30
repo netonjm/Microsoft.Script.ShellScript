00e34d3ccf3c762a609371055f625c44751d793a
José Medrano <jose.medrano@xamarin.com>
2016-12-30 06:34:48 +0100

    fixes library name later changes

--- make.sh
+++ make.sh
@@ -8,12 +8,12 @@ if [ ! -f ./tools/nuget.exe ]; then
     mv nuget.exe tools/
 fi
 echo "Restoring nugets packages..."
-mono tools/nuget.exe restore src/Xamarin.ShellScript.sln
+mono tools/nuget.exe restore src/Microsoft.Script.ShellScript.sln
 
 echo "build project in release mode"
-xbuild /p:Configuration=Release src/Xamarin.ShellScript/Xamarin.ShellScript.csproj 
+xbuild /p:Configuration=Release src/Microsoft.Script.ShellScript/Microsoft.Script.ShellScript.csproj 
 
 echo "Generating nuget..."
-mono tools/nuget.exe pack templates/nuget/Xamarin.ShellScript.nuspec -outputdirectory templates/nuget/packages/
+mono tools/nuget.exe pack templates/nuget/Microsoft.Script.ShellScript.nuspec -outputdirectory templates/nuget/packages/
 
 echo "Finished."
